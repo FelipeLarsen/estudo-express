d55a170c85feccf84566d3a35ed0ab269c22deec
FelipeLarsen <feliper.larsen@gmail.com>
2024-07-26 14:43:46 -0300

    just the basic

--- myapp/app.js
+++ myapp/app.js
@@ -6,22 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// 
-// app.use(express.static('public'));
-// app.use('/static', express.static('public'));
-
 app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
-
-// app.use(function (err, req, res, next) {
-//   console.error(err.stack);
-//   res.status(500).send('Something broke!');
-// });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
